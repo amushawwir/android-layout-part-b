0052aba77dd18606ebd0c0bc24c7af23b9241428
amushawwir <mushawwir123@gmail.com>
2022-09-22 19:57:55 +0700

    first commit

---
+++ app/src/main/java/jti/andi/HelloConstraint.java
@@ -0,0 +1,14 @@
+package jti.andi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelloConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_constraint);
+    }
+}
